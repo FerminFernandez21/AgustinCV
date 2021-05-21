a0ac7d7ae0304b3ac6dacd0e99d261d227a73ae3
FerminFernandez21 <fermin.f.b@hotmail.com>
2021-05-21 11:54:24 -0300

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { InfoComponent } from './components/info/info.component';
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
